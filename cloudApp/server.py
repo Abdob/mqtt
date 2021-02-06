a2f0381c4b4fb8ab3465e58e24e4805c40ba94ef
Abdo Babukr <abdo.babukr@gmail.com>
2021-02-06 08:42:33 -0800

    original files for cloud

---
+++ cloudApp/server.py
@@ -0,0 +1,47 @@
+import base64
+import cv2 as cv
+import numpy as np
+import paho.mqtt.client as mqtt
+import os
+from datetime import datetime
+
+MQTT_BROKER = os.getenv('MQTT')
+MQTT_RECEIVE = "streaming"
+
+frame = np.zeros((240, 320, 3), np.uint8)
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(MQTT_RECEIVE)
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    global frame
+    # Decoding the message
+    save_string = 'face_' + str(datetime.now()) + '.bin'
+    print("Storing face ..")
+    f = open(save_string, 'w+b')
+    f.write(msg.payload)
+    f.close()
+    # converting into numpy array from buffer
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(MQTT_BROKER)
+
+# Starting thread which will receive the frames
+client.loop_start()
+
+while 1:
+    i=0
+# Stop the Thread
+client.loop_stop()
